0b488d162d17661feb713b9891332666702f0daf
SoeHtut <soehtut@escapepixel.com>
2021-01-20 23:02:25 +0630

    Template Refs in the Setup Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   setup() {
     let name = 'John'
     let age = 30
 
+    const p = ref(null)
+
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.textContent = 'hello dojo'
     }
 
     // return { name: name, age: age}
-    return { name, age, handleClick}
+    return { name, age, handleClick, p}
   }
 }
 </script>
