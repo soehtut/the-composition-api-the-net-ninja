7b3988e0f87a57d79f4523a9852c4b1e4abb7580
SoeHtut <soehtut@escapepixel.com>
2021-01-28 21:32:52 +0630

    Creating a Tag Cloud

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -13,12 +14,13 @@
 <script>
 import { ref, reactive, computed, watch, watchEffect } from 'vue'
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 import Spinner from '../components/Spinner.vue'
 import getPosts from '../composables/getPosts'
 
 export default {
   name: 'Home',
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
 
     const { posts, error, load } = getPosts()
@@ -35,4 +37,9 @@ export default {
     margin: 0 auto;
     padding: 10px;
   }
+  /* .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
+  } */
 </style>
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="tag">
         <div v-if="error">{{ error }}</div>
-        <div v-if="posts.length">
+        <div v-if="posts.length" class="layout">
             <PostList :posts="postsWithTag" />
+            <TagCloud :posts="posts" />
         </div>
         <div v-else>
             <Spinner />
@@ -12,13 +13,14 @@
 
 <script>
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 import Spinner from '../components/Spinner.vue'
 import getPosts from '../composables/getPosts'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default {
-    components: { PostList, Spinner },
+    components: { PostList, Spinner, TagCloud },
     setup() {
         const route = useRoute()
         const { posts, error, load } = getPosts()
@@ -31,3 +33,11 @@ export default {
     }
 }
 </script>
+
+<style>
+    .tag {
+        width: 1200px;
+        margin: 0 auto;
+        padding: 10px;
+    }
+</style>
