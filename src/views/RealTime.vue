87fad0d2280bdf2cd8f918281408903e55cddcdf
SoeHtut <soehtut@escapepixel.com>
2021-01-30 16:36:50 +0630

    Intro to Real-Time Listeners

---
+++ src/views/RealTime.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Real-time data</h1> 
+    <div v-for="post in posts" :key="post">{{ post.title }}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config';
+
+export default {
+    setup() {
+        const posts = ref([])
+
+        projectFirestore.collection('posts')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((snap) => {
+                let docs = snap.docs.map(doc => {
+                    return { ...doc.data(), id: doc.id}
+                })
+                posts.value = docs
+            })
+
+        return { posts }
+    }
+}
+</script>
